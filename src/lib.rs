c4d08d2099cc7a2c5248298f1634ebbd72f8eb97
John Nystrom <eta077@yahoo.com>
2023-05-17 18:05:33 -0500

    checkpoint for earendel work

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,19 @@
 #![deny(clippy::all)]
 #![doc = include_str!("../README.md")]
 
-use chrono::{NaiveDate, Utc};
+use astro_rs::coordinates::Icrs;
+use chrono::{DateTime, NaiveDate, Utc};
+
+use reqwest::header::HeaderMap;
+use reqwest::header::{ACCEPT, CONTENT_TYPE};
 
 use serde::{Deserialize, Serialize};
 
+use tracing::instrument;
+
+use uom::si::angle::degree;
+
+use std::env;
 use std::error::Error;
 
 /// Information used to display the APOD.
@@ -30,6 +39,73 @@ struct Apod {
     service_version: Option<String>,
     title: String,
     url: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct MastRequestParams {
+    ra: f64,
+    dec: f64,
+    radius: f64,
+}
+
+impl MastRequestParams {
+    pub fn to_urlencoded(&self) -> String {
+        let result = [
+            ("ra", &self.ra.to_string()),
+            ("dec", &self.dec.to_string()),
+            ("radius", &self.radius.to_string()),
+        ];
+        serde_urlencoded::to_string(&result).unwrap()
+    }
+}
+
+impl From<Icrs> for MastRequestParams {
+    fn from(value: Icrs) -> Self {
+        MastRequestParams {
+            ra: value.coords.ra.get::<degree>(),
+            dec: value.coords.dec.get::<degree>(),
+            radius: 0.2,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct MastRequest {
+    service: String,
+    params: MastRequestParams,
+    format: String,
+    pagesize: usize,
+    removenullcolumns: bool,
+    timeout: u32,
+    cachebreaker: DateTime<Utc>,
+}
+
+impl MastRequest {
+    pub fn new(params: MastRequestParams) -> Self {
+        MastRequest {
+            service: String::from("Mast.Caom.Cone"),
+            params,
+            format: String::from("json"),
+            pagesize: 25,
+            removenullcolumns: true,
+            timeout: 30,
+            cachebreaker: Utc::now(),
+        }
+    }
+
+    pub fn to_urlencoded(&self) -> String {
+        let result = [
+            ("service", &self.service),
+            ("params", &self.params.to_urlencoded()),
+            ("format", &self.format),
+            ("pagesize", &self.pagesize.to_string()),
+            ("removenullcolumns", &self.removenullcolumns.to_string()),
+            ("timeout", &self.timeout.to_string()),
+            ("cachebreaker", &self.cachebreaker.to_string()),
+        ];
+
+        serde_urlencoded::to_string(&result).unwrap()
+    }
 }
 
 /// The manager of the Earendel functionality and state.
@@ -58,7 +134,7 @@ impl EarendelServer {
 
     async fn fetch_apod_image() -> Result<EarendelState, Box<dyn Error>> {
         let api_url = "https://api.nasa.gov/planetary/apod";
-        let api_key = std::env::var("EARENDEL_API_KEY")?;
+        let api_key = env::var("EARENDEL_APOD_API_KEY")?;
         let request_url = [api_url, "?api_key=", &api_key].concat();
 
         let resp = reqwest::get(request_url).await?;
@@ -73,5 +149,56 @@ impl EarendelServer {
             img: img.to_vec(),
             copyright: apod.copyright,
         })
+    }
+
+    /// Gets FITS files for the current APOD. Returns an error if the web request fails.
+    ///
+    /// ```
+    /// use earendel::*;
+    ///
+    /// # tokio_test::block_on(async {
+    /// let server = EarendelServer::new();
+    /// server.get_fits_for_apod().await.unwrap();
+    /// # });
+    /// ```
+    #[instrument(skip(self))]
+    pub async fn get_fits_for_apod(&self) -> Result<(), Box<dyn Error>> {
+        let api_url = "https://mast.stsci.edu/api/v0/invoke";
+        let api_key = env::var("EARENDEL_MAST_API_KEY")?;
+
+        let coords = astro_rs::coordinates::lookup_by_name("NGC 1566").await?;
+
+        let params = MastRequestParams::from(coords);
+        let request = MastRequest::new(params);
+
+        println!("{request:?}");
+        //let encoded_request = ["request=", &request.to_urlencoded()].concat();
+        let encoded_request =
+            serde_urlencoded::to_string(&[("request", request.to_urlencoded())]).unwrap();
+        println!("encoded request: {:?}", encoded_request);
+
+        let client = reqwest::Client::new();
+
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            CONTENT_TYPE,
+            "application/x-www-form-urlencoded".parse().unwrap(),
+        );
+        headers.insert(ACCEPT, "text/plain".parse().unwrap());
+
+        let resp = client
+            .get(api_url)
+            .headers(headers)
+            .body(encoded_request)
+            .send()
+            .await?;
+
+        println!("response: {resp:?}");
+
+        let body = resp.text().await?;
+
+        println!("body: {body}");
+
+        Ok(())
     }
 }
